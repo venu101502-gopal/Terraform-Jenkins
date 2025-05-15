f14e0cdc23da967ddb9c8d39f96e18cc467d688a
venu101502-gopal <venuajet101502@gmail.com>
2025-05-15 19:42:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "venugopal" {
-  ami                    = "ami-0f9de6e2d2f067fca"
+  ami                    = "ami-084568db4383264d4"
   instance_type          = "t2.micro"
   key_name               = "Blue-key"  # Do NOT include .pem
   subnet_id              = "subnet-0d1fa72421efd7af5"
